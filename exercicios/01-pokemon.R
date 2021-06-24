44b0d7b99e5db3b68d5577dc3886cd0b72118a35
jtrecenti <julio.trecenti@gmail.com>
2021-06-24 18:40:48 -0300

    correção do enunciado do exercício

--- exercicios/01-pokemon.R
+++ exercicios/01-pokemon.R
@@ -11,6 +11,6 @@ u_base <- "https://pokeapi.co/api/v2/"
 # Dica: você pode fazer isso manualmente ou com um código em R, usando {purrr}
 
 # 3. Crie um data.frame com os 20 primeiros pokemons do tipo "grass"
-# Dica: utilize o parâmetro query=.
+# Dica: nesse caso, não dá para utilizar o parâmetro limit=""
 # Além disso, tabelas ficam mais fáceis de visualizar quando rodamos 
 # tibble::as_tibble(tab)
