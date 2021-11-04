cea542d597f3741ca35d72aedd776fe2251d65a9
jtrecenti <julio.trecenti@gmail.com>
2021-11-04 17:53:01 -0300

    precisei colocar SSL

--- exemplos_de_aula/02-sabesp.R
+++ exemplos_de_aula/02-sabesp.R
@@ -1,6 +1,6 @@
 # 1. colete os dados de mananciais da sabesp
 u_sabesp <- "http://mananciais.sabesp.com.br/api/Mananciais/ResumoSistemas/2020-03-15"
-r_sabesp <- httr::GET(u_sabesp)
+r_sabesp <- httr::GET(u_sabesp, httr::config(ssl_verifypeer = FALSE))
 results <- httr::content(r_sabesp, simplifyDataFrame = TRUE)
 results$ReturnObj$sistemas
 
