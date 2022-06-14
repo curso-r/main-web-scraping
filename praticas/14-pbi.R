5170c1b443ce5058aafa1f42d923d6e8a8c5b3fd
Julio Trecenti <julio.trecenti@gmail.com>
2022-06-14 08:46:42 -0300

    Update 14-pbi.R

--- praticas/14-pbi.R
+++ praticas/14-pbi.R
@@ -2,5 +2,7 @@ library(RSelenium)
 
 driver <- rsDriver(browser = "firefox")
 u <- "https://paineis.cnj.jus.br/QvAJAXZfc/opendoc.htm?document=qvw_l%2FPainelCNJ.qvw&host=QVS%40neodimio03&anonymous=true&sheet=shPDPrincipal"
+u <- "https://painel-estatistica.stg.cloud.cnj.jus.br/estatisticas.html"
+u <- "https://paineis.cnj.jus.br/QvAJAXZfc/opendoc.htm?document=qvw_l%2FPainelCNJ.qvw&host=QVS%40neodimio03&anonymous=true&sheet=shResumoDespFT"
 
 driver$client$navigate(u)
